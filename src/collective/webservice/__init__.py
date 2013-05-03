b671116322d2deadd62c7aeb64a6047b5c68f735
Clayton Caetano de Sousa <claytonc.sousa@gmail.com>
2013-05-03 14:35:26 -0300

    Update __init__.py

--- src/collective/webservice/__init__.py
+++ src/collective/webservice/__init__.py
@@ -1,5 +1,7 @@
 # -*- extra stuff goes here -*-
 from zope.i18nmessageid import MessageFactory
+from AccessControl import ModuleSecurityInfo
+from AccessControl import allow_class, allow_module
 
 # Define a message factory for when this product is internationalised.
 # This will be imported with the special name "_" in most modules. Strings
@@ -11,3 +13,93 @@ WebserviceMessageFactory = MessageFactory('collective.webservice')
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
 
+    tipos = """
+        voidType
+        stringType
+        untypedType
+        IDType
+        NCNameType
+        NameType
+        ENTITYType
+        IDREFType
+        languageType
+        NMTOKENType
+        QNameType
+        tokenType
+        normalizedStringType
+        CDATAType
+        booleanType
+        decimalType
+        floatType
+        doubleType
+        durationType
+        timeDurationType
+        dateTimeType
+        recurringInstantType
+        timeInstantType
+        timePeriodType
+        timeType
+        dateType
+        gYearMonthType
+        gYearType
+        centuryType
+        yearType
+        gMonthDayType
+        recurringDateType
+        gMonthType
+        monthType
+        gDayType
+        recurringDayType
+        hexBinaryType
+        base64BinaryType
+        base64Type
+        binaryType
+        anyURIType
+        uriType
+        uriReferenceType
+        NOTATIONType
+        ENTITIESType
+        IDREFSType
+        NMTOKENSType
+        integerType
+        nonPositiveIntegerType
+        non_Positive_IntegerType
+        negativeIntegerType
+        negative_IntegerType
+        longType
+        intType
+        shortType
+        byteType
+        nonNegativeIntegerType
+        non_Negative_IntegerType
+        unsignedLongType
+        unsignedIntType
+        unsignedShortType
+        unsignedByteType
+        positiveIntegerType
+        positive_IntegerType
+        compoundType
+        structType
+        headerType
+        bodyType
+        arrayType
+        typedArrayType
+    """
+
+    tipos = [t.strip() for t in tipos.split('\n') if t.strip()]
+    
+    product_globals = globals()
+    
+    for t in tipos:
+        dotted_name = 'SOAPpy.Types.' + t
+        parts = dotted_name.split('.')
+        m_name = '.'.join(parts[:-1])
+        k_name = parts[-1]
+        ModuleSecurityInfo(m_name).declarePublic(t)
+        module = __import__(m_name, product_globals, locals(), [k_name])
+        klass = getattr(module, k_name)
+        allow_class(klass)
+    
+    allow_module('xml.parsers.expat')
+    
+    ModuleSecurityInfo('App.Common').declarePublic('rfc1123_date')
